c0be636710483e581e69b051324f994972e2fe0c
Richer Larivière <richerlariviere@gmail.com>
2016-04-08 10:15:27 -0400

    Added comments for the plugin configuration

--- mavproxy_interop.py
+++ mavproxy_interop.py
@@ -8,9 +8,13 @@ from MAVProxy.modules.lib import mp_module
 class InteropModule(mp_module.MPModule):
     def __init__(self, mpstate):
         super(InteropModule, self).__init__(mpstate, "interop", "interoperability module")
-
+        
+        # The drone/autopilot firmware ip address like 127.0.0.1
         self.ip = "127.0.0.1"
+        
+        # The port you want to use in order to send your json via UDP
         self.port = 5005
+        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def mavlink_packet(self, m):
